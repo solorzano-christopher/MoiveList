c7e9badc77765baad592dae5079778771c0e0d08
Christopher Solorzano <metalime09@outlook.com>
2019-09-12 13:03:25 -0700

    Add project files.

---
+++ Ex1C - Cloud Apps/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ex1C___Cloud_Apps.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ex1C___Cloud_Apps.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Ex1C - Cloud Apps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ex1C___Cloud_Apps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
